d82110e227ccbcb0de70e2de1810c057d6f1bb50
chaozhu <473111298@qq.com>
2019-04-08 17:00:02 +0800

    简单工厂模式完成

--- src/gongchangmoshi/Factory.java
+++ src/gongchangmoshi/Factory.java
@@ -1,4 +1,40 @@
 package gongchangmoshi;
 
+
 public class Factory {
+    public Shape getShape(String shapeType){
+        if(shapeType == null){
+            return null;
+        }else if ("circle".equals(shapeType)){
+            return new Circle();
+        }else if("rectangle".equals(shapeType)){
+            return new Rectangle();
+        }else if("square".equals(shapeType)){
+            return new Square();
+        }
+        return null;
+    }
+}
+
+class Circle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("you draw a circle");
+    }
+}
+
+class Rectangle implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("you draw a rectangle");
+    }
+}
+
+class Square implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("you draw a Square ");
+    }
 }
